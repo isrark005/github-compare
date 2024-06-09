f485f8aa7f9d17b8b01f0cedefe4706dd1f45f57
isrark005 <isrark005@gmail.com>
2024-06-09 15:28:42 +0530

    changed HTTP tp HTTPS

--- src/APIs/getProfileData.jsx
+++ src/APIs/getProfileData.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const GetProfileData = async (userName) => {
   try {
     const response = await axios.get(
-      `http://github-contributions-api.jogruber.de/v4/${userName}`,
+      `https://github-contributions-api.jogruber.de/v4/${userName}`,
     );
     return response.data;
   } catch (error) {
